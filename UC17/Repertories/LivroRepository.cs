c8e837bcdaff4c99c627db68406c95c0e3f0acb0
Amanda <amandagabylopes23@gmail.com>
2023-01-04 18:03:15 -0300

    atualização enc2

---
+++ UC17/Repertories/LivroRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UC17.Contexts;
+using UC17.Models;
+
+namespace UC17.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastro(Livro l)
+        {
+            _context.Livros.Add(l);
+            _context.SaveChanges();
+        }
+        
+
+
+    }
+}
